b01ffc2234deadbaccc49f4802fb54dcb8967ea0
samuelhatar <samuel.hatar@gmail.com>
2025-06-24 20:32:55 +0200

    init commit

---
+++ src/test/java/SelTestTutorialpointCheck.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+public class SelTestTutorialpointCheck {
+
+private WebDriver ovladac;
+
+      @BeforeEach
+    public void setUp (){
+          System.setProperty("webdriver.chrome.driver", "C:\\Users\\samue\\OneDrive\\Documents\\SeleniumSkillmea\\src\\main\\resources\\chromedriver.exe");
+
+
+      }
+
+
+
+}
